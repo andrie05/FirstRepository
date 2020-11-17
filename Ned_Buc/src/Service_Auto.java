7f0b196d903261776c6a6ebe6e8aa67b652093e7
andrei05 <andrei.alex164@gmail.com>
2020-11-17 16:01:50 +0200

    Implementare schelet
    Definirea claselor Service,Angajat,Car
    
    Signed-off-by: andrei05 <andrei.alex164@gmail.com>

---
+++ Ned_Buc/src/Service_Auto.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Service_Auto {
+
+    public String Nume;
+    public String Adresa;
+    public ArrayList<Car> Lista_masini;
+    public ArrayList<Angajat> Lista_angajati;
+    private int Nr_Angajati;
+
+}
