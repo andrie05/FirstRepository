36a381ec6c591ae44d50e41cfb776f56d55a8687
andrei05 <andrei.alex164@gmail.com>
2020-11-17 16:20:21 +0200

    Implementare Constructori
    Signed-off-by: andrei05 <andrei.alex164@gmail.com>

--- Ned_Buc/src/Angajat.java
+++ Ned_Buc/src/Angajat.java
@@ -1,5 +1,12 @@
 public class Angajat {
     public String Nume;
+
+    public Angajat(String nume, String prenume, String CNP) {
+        Nume = nume;
+        Prenume = prenume;
+        this.CNP = CNP;
+    }
+
     public String Prenume;
     public String CNP;
 }
--- Ned_Buc/src/Service_Auto.java
+++ Ned_Buc/src/Service_Auto.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 
 public class Service_Auto {
 
+    public Service_Auto(String nume, String adresa) {
+        Nume = nume;
+        Adresa = adresa;
+    }
+
     public String Nume;
     public String Adresa;
     public ArrayList<Car> Lista_masini;
