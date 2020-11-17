d429f12051ee9a0b5896ae56929f686f801aa6b8
andrei05 <andrei.alex164@gmail.com>
2020-11-17 16:20:21 +0200

    Implementare Constructori
    Signed-off-by: andrei05 <andrei.alex164@gmail.com>

--- Ned_Buc/src/Car.java
+++ Ned_Buc/src/Car.java
@@ -1,4 +1,11 @@
 public class Car {
+    public Car(String nr_inmatriculare, String serie, String culoare, String an) {
+        Nr_inmatriculare = nr_inmatriculare;
+        Serie = serie;
+        Culoare = culoare;
+        An = an;
+    }
+
     public String Nr_inmatriculare;
     public String Serie;
     public String Culoare;
